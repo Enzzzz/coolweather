a0a0e30ccd5b665bb5b3262a0988ee49f9e33064
ENZE <1379565717@qq.com>
2018-04-03 09:57:32 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Enze on 2018-03-28.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
